977cdfb4294b45246d31aeae29205cc86b42bb7e
Eliú Timaná <EliuTimana@gmail.com>
2021-02-09 21:51:04 -0500

    Updates

---
+++ precache-manifest.422e11c278214bf79d6a5882a9d38933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098fcc36986d4b501b27",
+    "url": "/css/app.fa6c3906.css"
+  },
+  {
+    "revision": "082e01ac8c82e7f39df8dbb973d42648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d565b7f8a309d88626a",
+    "url": "/js/about.6ca581cb.js"
+  },
+  {
+    "revision": "098fcc36986d4b501b27",
+    "url": "/js/app.e8f60d0c.js"
+  },
+  {
+    "revision": "f8e585855cbb4c1ae283",
+    "url": "/js/chunk-vendors.7782987f.js"
+  },
+  {
+    "revision": "443af11d8a1785064c507f8c8eb5772c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
